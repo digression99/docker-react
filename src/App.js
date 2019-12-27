9aaba50f1721951c1cb1504074d3bc63414f40c5
Pseudocoder Kim <digression99@gmail.com>
2019-12-27 15:08:35 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>You removed the image! Haha! This is crazy! Awesome!</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
